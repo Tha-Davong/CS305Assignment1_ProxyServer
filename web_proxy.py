ca5775d19de51216d08c1c8c70bd6b043010aca6
Tha Davong <11910227@mail.sustech.edu.cn>
2021-10-01 16:19:42 +0700

    Multi Threading working

--- web_proxy.py
+++ web_proxy.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
-#NOTE: you are NOT allowed to use the "requests" library in python in the assignment
+# NOTE: you are NOT allowed to use the "requests" library in python in the assignment
 
-#default IP and PORT
+# default IP and PORT
 import socket
 import threading
 
 import requests
+
 
 class Proxy(threading.Thread):
     def __init__(self, conn, address):
         threading.Thread.__init__(self)
         self.Proxy_client_conn = conn
         self.address = address
+
     def run(self):
         data = self.Proxy_client_conn.recv(bufferSize)
         data_decode = data.decode().split('\r\n')
@@ -32,13 +34,16 @@ class Proxy(threading.Thread):
             if (len(web_reponse) > 0):
                 self.Proxy_client_conn.send(web_reponse)
 
+
 IP = '127.0.0.1'
 PORT = 8080
 bufferSize = 2048
+
+
 def proxy():
     # Create a server socket
     ProxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #bind it to the IP and Port,start listening and work
+    # bind it to the IP and Port,start listening and work
     # Fill in start.
     ProxySocket.bind((IP, PORT))
     ProxySocket.listen(10)
@@ -50,9 +55,7 @@ def proxy():
         # client to proxy
         Proxy_client_conn, addr = ProxySocket.accept()
         print('Received a connection from:', addr)
-        Proxy(Proxy_client_conn, addr)
-
-
+        Proxy(Proxy_client_conn, addr).start()
 
 
 if __name__ == "__main__":
