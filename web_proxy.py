00fbd9ce87bc21012830f6b062dbe476d7df3d68
Tha Davong <11910227@mail.sustech.edu.cn>
2021-10-01 15:38:49 +0700

    done GET, HEAD and POST

--- web_proxy.py
+++ web_proxy.py
@@ -1,41 +1,53 @@
 # -*- coding: utf-8 -*-
 
 #NOTE: you are NOT allowed to use the "requests" library in python in the assignment
-from socket import *
 
 #default IP and PORT
+import socket
+import requests
+
 IP = '127.0.0.1'
 PORT = 8080
-
-hello = [b'HTTP/1.0 200 OK\r\n', b'Connection: close' b'Content-Type:text/html; charset=utf-8\r\n', b'\r\n',
-         b'<html><body>Hello World!<body></html>\r\n', b'\r\n']
-err404 = [b'HTTP/1.0 404 Not Found\r\n', b'Connection: close' b'Content-Type:text/html; charset=utf-8\r\n', b'\r\n',
-          b'<html><body>404 Not Found<body></html>\r\n', b'\r\n']
+bufferSize = 2048
 def proxy():
     # Create a server socket
-    ProxySocket = socket(AF_INET, SOCK_STREAM)
+    ProxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #bind it to the IP and Port,start listening and work
     # Fill in start.
     ProxySocket.bind((IP, PORT))
     ProxySocket.listen(10)
+
     # Fill in end.
     while 1:
         # Strat receiving data from the client
         print('Ready to serve...')
+        # client to proxy
         Proxy_client_conn, addr = ProxySocket.accept()
         print('Received a connection from:', addr)
         # Proxy the request from the clinet
         # Fill in start.
-        data = Proxy_client_conn.recv(2048).decode().split('\r\n')
-        print(data[0].split(' '))
-        res = err404
-        if data[0].split(' ')[1] == '/':
-            res = hello
-        for line in res:
-            Proxy_client_conn.send(line)
+        data = Proxy_client_conn.recv(bufferSize)
+        data_decode = data.decode().split('\r\n')
+        #proxy to web
+        web = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        webserver = data_decode[1].split(' ')[1]
+        web.connect((webserver, 80))
+        web.send(data)
+        while 1:
+            web.settimeout(2)
+            try:
+                web_reponse = web.recv(bufferSize)
+            except socket.error:
+                break
+
+            if (len(web_reponse) > 0):
+                Proxy_client_conn.send(web_reponse)
         Proxy_client_conn.close()
+        web.close()
+
         # Fill in end.
-        Proxy_client_conn.close()
+
 
 # Fill in start.
 
